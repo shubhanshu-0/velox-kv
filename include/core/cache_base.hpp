fc85681c918ee5bf536f19a005367191a6e57070
shubhanshu-0 <shubhanshusaraf0@gmail.com>
2026-04-03 04:51:26 +0530

    Cache Base Class updated; evict() as virtual function; lru policy bugfix - set()

--- include/core/cache_base.hpp
+++ include/core/cache_base.hpp
@@ -3,36 +3,34 @@
 #include <iostream>
 #include <map>
 #include <memory>
-#include <stdexcept>
-#include <thread>
 #include <mutex>
-#include <string>
 #include <optional>
+#include <stdexcept>
+#include <string>
+#include <thread>
 
 template <typename K, typename V>
 /* DLL */
-class Node
-{
+class Node {
 public:
-    K key;
-    V value;
-    Node *prev;
-    Node *next;
-    bool is_sentinel;
+  K key;
+  V value;
+  Node *prev;
+  Node *next;
+  bool is_sentinel;
 
-    Node() : prev(nullptr), next(nullptr), is_sentinel(true) {}
-    
-    Node(K k, V v) : key(k), value(v), prev(nullptr), next(nullptr), is_sentinel(false) {}
+  Node() : prev(nullptr), next(nullptr), is_sentinel(true) {}
+
+  Node(K k, V v)
+      : key(k), value(v), prev(nullptr), next(nullptr), is_sentinel(false) {}
 };
 
-template <typename K, typename V>
-class Cache_Operations
-{
-    public:
-        uint32_t capacity;
-        Cache_Operations(uint32_t cap) : capacity(cap) {}
-        virtual ~Cache_Operations() = default;
-        virtual std::optional<V> get(K key) = 0;
-        virtual bool set(K key, V value) = 0;
-        virtual bool remove(K key) = 0;
+template <typename K, typename V> class Cache {
+public:
+  uint32_t capacity;
+  Cache(uint32_t cap) : capacity(cap) {}
+  virtual ~Cache() = default;
+  virtual std::optional<V> get(K key) = 0;
+  virtual bool set(K key, V value) = 0;
+  virtual bool remove(K key) = 0;
 };
--- include/core/eviction_policy.hpp
+++ include/core/eviction_policy.hpp
@@ -2,18 +2,21 @@
 
 #include "cache_base.hpp"
 
-template <typename K, typename V>
-class Eviction_Policy
-{
+template <typename K, typename V> class EvictionPolicy {
 public:
-    void evict(Node<K, V> *ref_node, std::map<K, std::shared_ptr<Node<K, V>>>& cache_map);
+  virtual void evict(Node<K, V> *ref_node,
+                     std::map<K, std::shared_ptr<Node<K, V>>> &cache_map);
+  virtual ~EvictionPolicy() = default;
 };
 
 template <typename K, typename V>
-inline void Eviction_Policy<K, V>::evict(Node<K, V> *tail, std::map<K, std::shared_ptr<Node<K, V>>>& cache_map)
-{
-        Node<K, V>* prev_node = tail->prev;
-        tail->prev = prev_node->prev;
-        prev_node->prev->next = tail;
-        cache_map.erase(prev_node->key);
+void EvictionPolicy<K, V>::evict(
+    Node<K, V> *tail, std::map<K, std::shared_ptr<Node<K, V>>> &cache_map) {
+  Node<K, V> *prev_node = tail->prev;
+  K k = prev_node->key;
+  V v = prev_node->value;
+  tail->prev = prev_node->prev;
+  prev_node->prev->next = tail;
+  cache_map.erase(prev_node->key);
+  std::cout << "Cache Eviction Successfull ! " << k << " " << v << std::endl;
 }
